72709529f5d564871f6c745a35c8ef4eda5189b7
Tom <98980532+Aluno-TomBr@users.noreply.github.com>
2022-02-04 22:46:21 -0300

    upload

--- p5play_template-main/sketch.js
+++ p5play_template-main/sketch.js
@@ -1,34 +1,66 @@
+var background;
 var box;
 
 function setup() {
-  createCanvas(400,400);
+  createCanvas(400,400)
 
-  box = createSprite (200,200,20,20);
+background = createSprite (200, 200, 400,400)
+box = createSprite (200,200,20,20);
 }
+
+
 
 function draw() 
 {
-  background(30);
-
   if (keyIsDown (RIGHT_ARROW)){
+  
+    background.shapeColor = "blue";
+
+box.shapeColor = "green";
 
     box.position.x = box.position.x +5;
   }
-  if (keyIsDown (LEFT_ARROW)){
+ if (keyIsDown (LEFT_ARROW)){
+  
+  background.shapeColor = "green";
 
+ box.shapeColor = "blue";
+    
     box.position.x = box.position.x -5;
   }
-  if (keyIsDown (UP_ARROW)){
+ if (keyIsDown (UP_ARROW)){
+
+  background.shapeColor = "red";
+
+    box.shapeColor = "yellow";
 
     box.position.y = box.position.y -5;
   }
   if (keyIsDown (DOWN_ARROW)){
+   
+    background.shapeColor = "yellow";
 
-    box.position.y = box.position.y +5;
+     box.shapeColor = "red";
+      
+      box.position.y = box.position.y +5;
   }
+  if (box.position.x >= 390 ){
+
+   box.position.x = 390
+  } 
+  if (box.position.x <= 10){
+
+    box.position.x = 10
+  }
+  if (box.position.y >= 390){
+
+    box.position.y = 390
+   } 
+   if (box.position.y <= 10){
+ 
+     box.position.y = 10
+   }
+
+
   drawSprites();
 }
-
-
-
-
